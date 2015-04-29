da71ff39d986edc3e07205a3623224663604a462
sliss <steven@stevenliss.com>
2015-04-29 02:59:11 -0400

    added old calendar frame, time-dynamic background gradient

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Reunions "Advent" Calendar' });
+};
+
+exports.day = function(req, res){
+    console.log('params:', req.params);
+    res.render('index', { title: 'Reunions "Advent" Calendar' });
 };
