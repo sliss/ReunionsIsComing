63ecd47c37edb1d3e060966033fa570f3552e463
sliss <steven@stevenliss.com>
2015-04-29 04:03:40 -0400

    implmented time check, ensure link leads to a number

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,39 @@
  * GET home page.
  */
 
+
+var titles = {
+    1: 'Clockwork',
+    2: 'Oranges'
+};
+
+
+function timeCheck(day){
+    day = parseInt(day);
+    var allowedDate = new Date(2015,04, day);
+    var today = new Date();
+    if(today >= allowedDate){
+        return true;
+    }
+
+    return false;
+}
+
 exports.index = function(req, res){
     res.render('index', { title: 'Reunions "Advent" Calendar' });
 };
 
 exports.day = function(req, res){
-    console.log('params:', req.params);
-    res.render('index', { title: 'Reunions "Advent" Calendar' });
+    var day = req.params.day;
+    console.log('day type:', typeof day);
+    if(!isNaN(day) && day <= 28){
+        if(timeCheck(day)){
+            res.render(day, { title: titles[day] });
+        } else {
+            res.render('tooSoon', {title: 'For shame!'});
+        }
+    }
+    else {
+        res.redirect('/');
+    }
 };
